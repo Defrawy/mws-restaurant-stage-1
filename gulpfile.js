e4904744c89f52230a195ca70fb13f454872a874
Mahmoud <Mahmoud@Mahmouds-MacBook-Air.local>
2018-06-03 07:45:20 +0200

    =maniest added

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+
+var gulp = require('gulp');
+// var sass = require('gulp-sass');
+var autprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+ browserSync.init({
+     server: "./"
+ });
+ browserSync.stream();
+
+gulp.task('default', function () {
+  console.log('hello, gulp');
+});
+
+// glup.task('styles', function() {
+//   glup.src('sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions']
+//     }))
+//     .pipe(gulp.dest('/css'));
+// });
